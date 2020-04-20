38790014245e008891b308a2a6f34d13918d1b39
RyanDuong360 <ryanduong360@gmail.com>
2020-04-20 17:16:41 +1000

    creating java class

---
+++ app/src/main/java/com/example/groupassignment/Fish.java
@@ -0,0 +1,559 @@
+package com.example.groupassignment;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Fish {
+
+    @SerializedName("Fishery Management")
+    @Expose
+    private String fisheryManagement;
+    @SerializedName("Habitat")
+    @Expose
+    private String habitat;
+    @SerializedName("Habitat Impacts")
+    @Expose
+    private String habitatImpacts;
+    @SerializedName("Location")
+    @Expose
+    private String location;
+    @SerializedName("Management")
+    @Expose
+    private String management;
+    @SerializedName("NOAA Fisheries Region")
+    @Expose
+    private String nOAAFisheriesRegion;
+    @SerializedName("Population")
+    @Expose
+    private String population;
+    @SerializedName("Population Status")
+    @Expose
+    private String populationStatus;
+    @SerializedName("Scientific Name")
+    @Expose
+    private String scientificName;
+    @SerializedName("Species Aliases")
+    @Expose
+    private String speciesAliases;
+    @SerializedName("Species Name")
+    @Expose
+    private String speciesName;
+    @SerializedName("Animal Health")
+    @Expose
+    private String animalHealth;
+    @SerializedName("Availability")
+    @Expose
+    private String availability;
+    @SerializedName("Biology")
+    @Expose
+    private String biology;
+    @SerializedName("Bycatch")
+    @Expose
+    private String bycatch;
+    @SerializedName("Calories")
+    @Expose
+    private String calories;
+    @SerializedName("Carbohydrate")
+    @Expose
+    private String carbohydrate;
+    @SerializedName("Cholesterol")
+    @Expose
+    private String cholesterol;
+    @SerializedName("Color")
+    @Expose
+    private String color;
+    @SerializedName("Disease Treatment and Prevention")
+    @Expose
+    private String diseaseTreatmentAndPrevention;
+    @SerializedName("Diseases in Salmon")
+    @Expose
+    private String diseasesInSalmon;
+    @SerializedName("Displayed Seafood Profile Illustration")
+    @Expose
+    private String displayedSeafoodProfileIllustration;
+    @SerializedName("Ecosystem Services")
+    @Expose
+    private String ecosystemServices;
+    @SerializedName("Environmental Considerations")
+    @Expose
+    private String environmentalConsiderations;
+    @SerializedName("Environmental Effects")
+    @Expose
+    private String environmentalEffects;
+    @SerializedName("Farming Methods")
+    @Expose
+    private String farmingMethods;
+    @SerializedName("Fat, Total")
+    @Expose
+    private String fatTotal;
+    @SerializedName("Feeds")
+    @Expose
+    private String feeds;
+    @SerializedName("Fiber, Total Dietary")
+    @Expose
+    private String fiberTotalDietary;
+    @SerializedName("Fishing Rate")
+    @Expose
+    private String fishingRate;
+    @SerializedName("Harvest")
+    @Expose
+    private String harvest;
+    @SerializedName("Harvest Type")
+    @Expose
+    private String harvestType;
+    @SerializedName("Health Benefits")
+    @Expose
+    private String healthBenefits;
+    @SerializedName("Human Health")
+    @Expose
+    private String humanHealth;
+    @SerializedName("Physical Description")
+    @Expose
+    private String physicalDescription;
+    @SerializedName("Production")
+    @Expose
+    private String production;
+    @SerializedName("Protein")
+    @Expose
+    private String protein;
+    @SerializedName("Quote")
+    @Expose
+    private String quote;
+    @SerializedName("Quote Background Color")
+    @Expose
+    private String quoteBackgroundColor;
+    @SerializedName("Research")
+    @Expose
+    private String research;
+    @SerializedName("Saturated Fatty Acids, Total")
+    @Expose
+    private String saturatedFattyAcidsTotal;
+    @SerializedName("Selenium")
+    @Expose
+    private String selenium;
+    @SerializedName("Serving Weight")
+    @Expose
+    private String servingWeight;
+    @SerializedName("Servings")
+    @Expose
+    private String servings;
+    @SerializedName("Sodium")
+    @Expose
+    private String sodium;
+    @SerializedName("Source")
+    @Expose
+    private String source;
+    @SerializedName("Sugars, Total")
+    @Expose
+    private String sugarsTotal;
+    @SerializedName("Taste")
+    @Expose
+    private String taste;
+    @SerializedName("Texture")
+    @Expose
+    private String texture;
+    @SerializedName("Path")
+    @Expose
+    private String path;
+
+    public String getFisheryManagement() {
+        return fisheryManagement;
+    }
+
+    public void setFisheryManagement(String fisheryManagement) {
+        this.fisheryManagement = fisheryManagement;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getHabitatImpacts() {
+        return habitatImpacts;
+    }
+
+    public void setHabitatImpacts(String habitatImpacts) {
+        this.habitatImpacts = habitatImpacts;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getManagement() {
+        return management;
+    }
+
+    public void setManagement(String management) {
+        this.management = management;
+    }
+
+    public String getNOAAFisheriesRegion() {
+        return nOAAFisheriesRegion;
+    }
+
+    public void setNOAAFisheriesRegion(String nOAAFisheriesRegion) {
+        this.nOAAFisheriesRegion = nOAAFisheriesRegion;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getPopulationStatus() {
+        return populationStatus;
+    }
+
+    public void setPopulationStatus(String populationStatus) {
+        this.populationStatus = populationStatus;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getSpeciesAliases() {
+        return speciesAliases;
+    }
+
+    public void setSpeciesAliases(String speciesAliases) {
+        this.speciesAliases = speciesAliases;
+    }
+
+    public String getSpeciesName() {
+        return speciesName;
+    }
+
+    public void setSpeciesName(String speciesName) {
+        this.speciesName = speciesName;
+    }
+
+    public String getAnimalHealth() {
+        return animalHealth;
+    }
+
+    public void setAnimalHealth(String animalHealth) {
+        this.animalHealth = animalHealth;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public String getBiology() {
+        return biology;
+    }
+
+    public void setBiology(String biology) {
+        this.biology = biology;
+    }
+
+    public String getBycatch() {
+        return bycatch;
+    }
+
+    public void setBycatch(String bycatch) {
+        this.bycatch = bycatch;
+    }
+
+    public String getCalories() {
+        return calories;
+    }
+
+    public void setCalories(String calories) {
+        this.calories = calories;
+    }
+
+    public String getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(String carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public String getCholesterol() {
+        return cholesterol;
+    }
+
+    public void setCholesterol(String cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDiseaseTreatmentAndPrevention() {
+        return diseaseTreatmentAndPrevention;
+    }
+
+    public void setDiseaseTreatmentAndPrevention(String diseaseTreatmentAndPrevention) {
+        this.diseaseTreatmentAndPrevention = diseaseTreatmentAndPrevention;
+    }
+
+    public String getDiseasesInSalmon() {
+        return diseasesInSalmon;
+    }
+
+    public void setDiseasesInSalmon(String diseasesInSalmon) {
+        this.diseasesInSalmon = diseasesInSalmon;
+    }
+
+    public String getDisplayedSeafoodProfileIllustration() {
+        return displayedSeafoodProfileIllustration;
+    }
+
+    public void setDisplayedSeafoodProfileIllustration(String displayedSeafoodProfileIllustration) {
+        this.displayedSeafoodProfileIllustration = displayedSeafoodProfileIllustration;
+    }
+
+    public String getEcosystemServices() {
+        return ecosystemServices;
+    }
+
+    public void setEcosystemServices(String ecosystemServices) {
+        this.ecosystemServices = ecosystemServices;
+    }
+
+    public String getEnvironmentalConsiderations() {
+        return environmentalConsiderations;
+    }
+
+    public void setEnvironmentalConsiderations(String environmentalConsiderations) {
+        this.environmentalConsiderations = environmentalConsiderations;
+    }
+
+    public String getEnvironmentalEffects() {
+        return environmentalEffects;
+    }
+
+    public void setEnvironmentalEffects(String environmentalEffects) {
+        this.environmentalEffects = environmentalEffects;
+    }
+
+    public String getFarmingMethods() {
+        return farmingMethods;
+    }
+
+    public void setFarmingMethods(String farmingMethods) {
+        this.farmingMethods = farmingMethods;
+    }
+
+    public String getFatTotal() {
+        return fatTotal;
+    }
+
+    public void setFatTotal(String fatTotal) {
+        this.fatTotal = fatTotal;
+    }
+
+    public String getFeeds() {
+        return feeds;
+    }
+
+    public void setFeeds(String feeds) {
+        this.feeds = feeds;
+    }
+
+    public String getFiberTotalDietary() {
+        return fiberTotalDietary;
+    }
+
+    public void setFiberTotalDietary(String fiberTotalDietary) {
+        this.fiberTotalDietary = fiberTotalDietary;
+    }
+
+    public String getFishingRate() {
+        return fishingRate;
+    }
+
+    public void setFishingRate(String fishingRate) {
+        this.fishingRate = fishingRate;
+    }
+
+    public String getHarvest() {
+        return harvest;
+    }
+
+    public void setHarvest(String harvest) {
+        this.harvest = harvest;
+    }
+
+    public String getHarvestType() {
+        return harvestType;
+    }
+
+    public void setHarvestType(String harvestType) {
+        this.harvestType = harvestType;
+    }
+
+    public String getHealthBenefits() {
+        return healthBenefits;
+    }
+
+    public void setHealthBenefits(String healthBenefits) {
+        this.healthBenefits = healthBenefits;
+    }
+
+    public String getHumanHealth() {
+        return humanHealth;
+    }
+
+    public void setHumanHealth(String humanHealth) {
+        this.humanHealth = humanHealth;
+    }
+
+    public String getPhysicalDescription() {
+        return physicalDescription;
+    }
+
+    public void setPhysicalDescription(String physicalDescription) {
+        this.physicalDescription = physicalDescription;
+    }
+
+    public String getProduction() {
+        return production;
+    }
+
+    public void setProduction(String production) {
+        this.production = production;
+    }
+
+    public String getProtein() {
+        return protein;
+    }
+
+    public void setProtein(String protein) {
+        this.protein = protein;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getQuoteBackgroundColor() {
+        return quoteBackgroundColor;
+    }
+
+    public void setQuoteBackgroundColor(String quoteBackgroundColor) {
+        this.quoteBackgroundColor = quoteBackgroundColor;
+    }
+
+    public String getResearch() {
+        return research;
+    }
+
+    public void setResearch(String research) {
+        this.research = research;
+    }
+
+    public String getSaturatedFattyAcidsTotal() {
+        return saturatedFattyAcidsTotal;
+    }
+
+    public void setSaturatedFattyAcidsTotal(String saturatedFattyAcidsTotal) {
+        this.saturatedFattyAcidsTotal = saturatedFattyAcidsTotal;
+    }
+
+    public String getSelenium() {
+        return selenium;
+    }
+
+    public void setSelenium(String selenium) {
+        this.selenium = selenium;
+    }
+
+    public String getServingWeight() {
+        return servingWeight;
+    }
+
+    public void setServingWeight(String servingWeight) {
+        this.servingWeight = servingWeight;
+    }
+
+    public String getServings() {
+        return servings;
+    }
+
+    public void setServings(String servings) {
+        this.servings = servings;
+    }
+
+    public String getSodium() {
+        return sodium;
+    }
+
+    public void setSodium(String sodium) {
+        this.sodium = sodium;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getSugarsTotal() {
+        return sugarsTotal;
+    }
+
+    public void setSugarsTotal(String sugarsTotal) {
+        this.sugarsTotal = sugarsTotal;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public String getTexture() {
+        return texture;
+    }
+
+    public void setTexture(String texture) {
+        this.texture = texture;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+}
